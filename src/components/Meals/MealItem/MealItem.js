a2a509062af9341cd86cd803523683742d669681
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-09-13 12:11:09 +0100

    Adding meal item form and input

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -17,7 +18,7 @@ const MealItem = (props) => {
                 </div>
             </div>
             <div>
-
+                <MealItemForm />
             </div>
         </li>
     )
