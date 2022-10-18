a437a06e5e8081a36785b644886f8a955f4d3223
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-10-18 14:44:54 +0100

    corrections

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -11,7 +11,7 @@ const MealItemForm = (props) => {
         const enteredAmount = amoutnInputRef.current.value;
         const enteredAmountNumber = +enteredAmount;
 
-        if (enteredAmount.trim().length == 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
             setAmountIsValid(false);
             return;
         }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,9 +7,9 @@ const defaultCartState = {
     totalAmount: 0
 }
 const cartReducer = (state, action) => {
-    if (action.type == 'ADD') {
+    if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        const existingCartItemIndex = state.items.findIndex((item) => item.id == action.item.id);
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
         const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
