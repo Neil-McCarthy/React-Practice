d483d1a1ec2c6b980ba2b0b1072385975813bc01
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-10-19 09:49:56 +0100

    call get on load instead of on click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,8 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async() => {
     setIsLoading(true);
     setError(null);
     try {
@@ -24,13 +25,18 @@ function App() {
           openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date
         }
-      })
+      }, []);
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  });
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  
   const dummyMovies = [
     {
       id: 1,
